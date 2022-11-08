0f025850f7cdcf2f218a4138972f61aa8792adfd
Fatima <fawicoma@gmail.com>
2022-11-08 00:51:27 -0500

    Added Boards services, show boards and advance of create boards

--- src/pages/my-boards.js
+++ src/pages/my-boards.js
@@ -1,7 +1,68 @@
-const MyBoards = () => {
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import CreateBoard from "../components/create-board";
+import { Modal } from "../components/create-modal";
+import { getBoards } from "../services/board-services";
+
+const ContainerBoards = styled.div`
+  display: flex;
+`;
+
+const CardBoard = styled.div`
+  width: 190px;
+  height: 96px;
+  border: 1px solid black;
+  margin: 8px;
+  border-radius: 8px;
+`;
+
+const CreateBoardCard = styled.button`
+  width: 190px;
+  height: 96px;
+  border: 1px solid black;
+  margin: 8px;
+  border-radius: 8px;
+`;
+
+function MyBoards() {
+  const [boards, setBoards] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    getBoards()
+    .then(boards => setBoards(boards))
+    .catch(error => console.log(error))
+  }, [])
+
   return(
     <div>
-    <h1>My Boards</h1>
+      <h1>My Boards</h1>
+      <div>
+        <h2>Starred Boards</h2>
+        <ContainerBoards>
+          <CardBoard>
+            <p>Name Board</p>
+            <button>Star</button>
+            <button>Delete</button>
+          </CardBoard>
+        </ContainerBoards>
+      </div>
+      <div>
+        <h2>Boards</h2>
+        <ContainerBoards>
+          {boards.map((board) => {
+            return(
+              <CardBoard key={board.id}>
+                <p>{board.name}</p>
+                <button>Star</button>
+                <button>Delete</button>
+              </CardBoard>
+            )
+          })}
+          <Modal open={open} close={() => setOpen(false)}><CreateBoard/></Modal>
+          <CreateBoardCard onClick={() => setOpen(true)}>Create Board</CreateBoardCard>
+        </ContainerBoards>
+      </div>
     </div>
   )
 }
--- src/pages/my-profile.js
+++ src/pages/my-profile.js
@@ -1,7 +1,68 @@
+import { useState } from "react";
+import { Input } from "../components/input";
+import { useAuth } from "../context/auth-context";
+import { deleteUser, editUser } from "../services/user-services";
+
 function MyProfile() {
-  return(
+  
+  const { user } = useAuth();
+  const { logout } = useAuth();
+
+  const [formData, setFormData] = useState({
+    username: user.username,
+    email: user.email,
+    first_name: user.firstName,
+    last_name: user.lastName,
+  });
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(user.id, formData);
+    editUser(user.id, formData)
+  }
+
+  function handleChange(e) {
+    const {name, value} = e.target;
+    setFormData({ ... formData, [name]: value});
+  }
+
+  function handleDelete(){
+    deleteUser(user.id);
+    logout();
+  };
+
+  return (
     <div>
       <h1>My profile</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          label="Username"
+        />
+        <Input
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          label="Email"
+        />
+        <Input
+          name="first_name"
+          value={formData.first_name}
+          onChange={handleChange}
+          label="First name"
+        />
+        <Input
+          name="last_name"
+          value={formData.last_name}
+          onChange={handleChange}
+          label="Last name"
+        />
+        <button type="submit">Update Account</button>
+      </form>
+      <button onClick={handleDelete}>Delete my Account</button>
     </div>
   )
 }
